3aa413aac8d2a93be957e8d90aeb29bfd721115c
Victor Hugo Cretton Rivera <victorcrettonrivera@gmail.com>
2025-06-03 21:34:47 -0300

    TEST: Prueba para ver si anda la api 3

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,14 @@
-import { defineConfig, loadEnv } from 'vite'
+import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 import path from "path"
 
-// https://vitejs.dev/config/
-export default defineConfig(({ mode }) => {
-
-  return {
-    plugins: [react(), tailwindcss()],
-    resolve: {
-      alias: {
-        "@": path.resolve(__dirname, "./src"),
-      },
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react(), tailwindcss()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
     },
-  }
+  },
 })
