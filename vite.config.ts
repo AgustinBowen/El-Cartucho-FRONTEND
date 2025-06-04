f98bef48eae626e20a852132ce3d8540cd023ba4
Victor Hugo Cretton Rivera <victorcrettonrivera@gmail.com>
2025-06-03 21:10:35 -0300

    WIP: ENTORNO VIRTUAL CON API_URL

--- vite.config.ts
+++ vite.config.ts
@@ -1,22 +1,26 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 import path from "path"
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
+// https://vitejs.dev/config/
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd()) // ✅ carga las variables .env correctamente
+
+  return {
+    plugins: [react(), tailwindcss()],
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "./src"),
+      },
     },
-  },
-  server: {
-    proxy: {
-      '/ed': {
-        target: 'https://el-cartucho-git-dev-victor-s-projects-2bfad959.vercel.app',
-        changeOrigin: true,
-        secure: false
+    server: {
+      proxy: {
+        '/ed': {
+          target: env.VITE_API_URL, // ✅ usar process.env o env
+          changeOrigin: true,
+          secure: false
+        }
       }
     }
   }
