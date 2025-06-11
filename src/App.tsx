bbf314600bd8acd01d6a22135fe44d388ad0ffae
Victor Hugo Cretton Rivera <victorcrettonrivera@gmail.com>
2025-06-11 20:35:00 -0300

    Pagina de error y de success

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
 import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import { Home } from './screens/Home';
+import { SuccessScreen } from './screens/SuccessScreen';
+import { ErrorScreen } from './screens/ErrorScreen';
 import { Catalog } from './screens/Catalog';
 import { CartScreen } from './screens/CartScreen';
 import { CartProvider } from "./context/CartContext";
+
 
 function App() {
 	return (
@@ -14,7 +17,8 @@ function App() {
 					<Route path="/" element={<Home />} />
 					<Route path="/catalogo" element={<Catalog />} />
 					<Route path="/comprar" element={<CartScreen />} />
-					<Route path="/pago/success*" element={<div>Se realizo el pago correctamente</div>} />
+					<Route path="/pago/success*" element={<SuccessScreen />} />
+					<Route path="/*" element={<ErrorScreen />} />
 				</Routes>
 			</CartProvider>
 		</Router>
